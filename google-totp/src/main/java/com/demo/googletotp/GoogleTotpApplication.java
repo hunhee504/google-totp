1538db1cf501b9e65ab47357d9ad5b9c0551f435
hunhee504 <gnsgml504@3top.co.kr>
2025-02-12 17:27:57 +0900

    First Commit

---
+++ google-totp/src/main/java/com/demo/googletotp/GoogleTotpApplication.java
@@ -0,0 +1,13 @@
+package com.demo.googletotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleTotpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleTotpApplication.class, args);
+    }
+
+}
---
+++ google-totp/src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 장훈희
+  Date: 2025-02-12
+  Time: 오후 5:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
